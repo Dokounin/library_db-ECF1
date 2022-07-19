185c636a1a207deff5e40da95a68f83a2a492462
Vladyslav Dokounin <dyacenkonikita@gmail.com>
2022-07-19 22:02:33 +0200

    add data Users in database

--- src/Controller/DbTestController.php
+++ src/Controller/DbTestController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,8 +11,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class DbTestController extends AbstractController
 {
     #[Route('/db/test', name: 'app_db_test')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
-       exit();
+        //récupération du repository des users
+        $repository = $doctrine->getRepository(User::class);
+        // récupération de la liste complète de toutes les users
+        $user = $repository->findAll();
+        //inspection de la liste
+        dump($user);
+
+
+        exit();
     }
 }
--- src/DataFixtures/TestFixtures.php
+++ src/DataFixtures/TestFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ManagerRegistry;
@@ -19,6 +20,40 @@ class TestFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $faker = FakerFactory::create("fr_FR");
-        
+        $this->loadUser($manager, $faker);
+    }
+
+    public function loadUser(ObjectManager $manager, FakerGenerator $faker): void
+    {
+
+        // boucle pour créer 100 user aleatoire via faker
+        for ($i = 0; $i < 100; $i++) {
+
+            $user = new User();
+            $user->setEmail($faker->email());
+            $user->setPassword($faker->sha256());
+            $user->setRoles(['ROLE_EMPRUNTEUR']);
+            $user->setEnabled($faker->boolean());
+
+            $date = $faker->dateTimeBetween('-6 month', '+6 month');
+            // format : YYYY-mm-dd HH:ii:ss
+            $date = DateTimeImmutable::createFromFormat('Y-m-d H:i:s', "2022-{$date->format('m-d H:i:s')}");
+            // si la gestion de la date est trop compliquée, voici une alternative mais l'année changera en fonction de quand vous lancer le chargement des fixtures
+            // $date = $faker->dateTimeThisYear();
+            // $date = DateTimeImmutable::createFromInterface($date);
+            $user->setCreatedAt($date);
+            $date = $faker->dateTimeBetween('-6 month', '+6 month');
+            // format : YYYY-mm-dd HH:ii:ss
+            $date = DateTimeImmutable::createFromFormat('Y-m-d H:i:s', "2022-{$date->format('m-d H:i:s')}");
+            // si la gestion de la date est trop compliquée, voici une alternative mais l'année changera en fonction de quand vous lancer le chargement des fixtures
+            // $date = $faker->dateTimeThisYear();
+            // $date = DateTimeImmutable::createFromInterface($date);
+            $user->setUpdatedAt($date);
+
+
+
+            $manager->persist($user);
+        }
+        $manager->flush();
     }
 }
