5b5db6a01cde45ddd791a86fa6f3c8afc92df50a
Vladyslav Dokounin <dyacenkonikita@gmail.com>
2022-07-20 11:22:36 +0200

    modification file TestFixtures, add users indispensable

--- src/DataFixtures/TestFixtures.php
+++ src/DataFixtures/TestFixtures.php
@@ -25,14 +25,64 @@ class TestFixtures extends Fixture
 
     public function loadUser(ObjectManager $manager, FakerGenerator $faker): void
     {
+        $userDatas = 
+        [
+            [
+                "email" => "admin@example.com",
+                "roles" => ['ROLE_ADMIN'],
+                "password" => "$2y$10$/H2ChUxriH.0Q33g3EUEx.S2s4j/rGJH2G88jK9nCP60GbUW8mi5K",
+                "enabled" => true,
+                "created_at" => DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2020-01-01 09:00:00'),
+                "updated_at" => DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2020-01-01 09:00:00'),
+            ],
+            [
+                "email" => "foo.foo@example.com",
+                "roles" => ['ROLE_EMPRUNTEUR'],
+                "password" => "$2y$10$/H2ChUxriH.0Q33g3EUEx.S2s4j/rGJH2G88jK9nCP60GbUW8mi5K",
+                "enabled" => true,
+                "created_at" => DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2020-01-01 10:00:00'),
+                "updated_at" => DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2020-01-01 10:00:00'),
+            ],
+            [
+                "email" => "bar.bar@example.com",
+                "roles" => ['ROLE_EMPRUNTEUR'],
+                "password" => "$2y$10$/H2ChUxriH.0Q33g3EUEx.S2s4j/rGJH2G88jK9nCP60GbUW8mi5K",
+                "enabled" => false,
+                "created_at" => DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2020-02-01 11:00:00'),
+                "updated_at" => DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2020-05-01 12:00:00'),
+            ],
+            [
+                "email" => "baz.baz@example.com",
+                "roles" => ['ROLE_EMPRUNTEUR'],
+                "password" => "$2y$10$/H2ChUxriH.0Q33g3EUEx.S2s4j/rGJH2G88jK9nCP60GbUW8mi5K",
+                "enabled" => true,
+                "created_at" => DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2020-03-01 12:00:00'),
+                "updated_at" => DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2020-03-01 12:00:00'),
+            ],
+            
+        ];
+
+        foreach ($userDatas as $userData){
+            $user = new User();
+            $user->setEmail($userData['email']);
+            $user->setRoles($userData['roles']);
+            $user->setPassword($userData['password']);
+            $user->setEnabled($userData['enabled']);
+            $user->setCreatedAt($userData['created_at']);
+            $user->setUpdatedAt($userData['updated_at']);
+
+            $manager->persist($user);
+        }
+
+
 
         // boucle pour créer 100 user aleatoire via faker
         for ($i = 0; $i < 100; $i++) {
 
             $user = new User();
             $user->setEmail($faker->email());
-            $user->setPassword($faker->sha256());
             $user->setRoles(['ROLE_EMPRUNTEUR']);
+            $user->setPassword($faker->sha256());
             $user->setEnabled($faker->boolean());
 
             $date = $faker->dateTimeBetween('-6 month', '+6 month');
