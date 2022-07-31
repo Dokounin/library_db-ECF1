34ef9e8dd07577397154eccab6728414f092338a
Vladyslav Dokounin <dyacenkonikita@gmail.com>
2022-07-31 17:12:43 +0200

    update requests Livre

--- src/Controller/DbTestController.php
+++ src/Controller/DbTestController.php
@@ -75,7 +75,7 @@ class DbTestController extends AbstractController
     //todo          Les requêtes
 
     #[Route('/db/test/requests', name: 'app_db_test_requests')]
-    public function requests(UserRepository $userRepository, LivreRepository $livreRepository, GenreRepository $genreRepository, AuteurRepository $auteurRepository): Response
+    public function requests(UserRepository $userRepository, LivreRepository $livreRepository, GenreRepository $genreRepository, AuteurRepository $auteurRepository, ManagerRegistry $doctrine): Response
     {
         //! Les utilisateurs
 
@@ -99,39 +99,95 @@ class DbTestController extends AbstractController
 
         //! Les livres
 
+        //* Requêtes de lecture :
+
+        // la liste complète de tous les livres
         $livres = $livreRepository->findAll();
         dump($livres);
 
+        // les données du livre dont l'id est `1`
         $id = 1;
         $livre = $livreRepository->find($id);
         dump($livre);
 
+        // la liste des livres dont le titre contient le mot clé `lorem`
         $livre = $livreRepository->findByKeyword('Lorem');
         dump($livre);
 
-
+        // la liste des livres dont l'id de l'auteur est `2`
         $auteur = $auteurRepository->find(2);
         $livre = $auteur->getNom();
         $livre = $livreRepository->findByAuteur($auteur);
         dump($livre);
 
-
+        // la liste des livres dont le genre contient le mot clé `roman`
         $livres = $livreRepository->findByGenreKeyword('roman');
         dump($livres);
+
+        //* Listez tout les livres avec leur genre
+        // foreach ($livres as $livre) {
+        //     dump($livre);
+
+        //     $genres = $livre->getGenres();
+
+        //     foreach ($genres as $genre) {
+        //         dump($genre);
+        //     }
+        // }
+
+
+        // * Requêtes de création :
+
+        // création d'un nouvel objet Livre
+        $livre1 = new Livre();
+        $livre1->setTitre('Totum autem id externum');
+        $livre1->setAnneEdition(2020);
+        $livre1->setNombresPages(300);
+        $livre1->setCodeIsbn('9790412882714');
+        $auteur = $auteurRepository->find(2);
+        $livre1->setAuteur($auteur);
+        $genre = $genreRepository->find(6);
+        $livre1->addGenre($genre);
+
+        // récupération de l'Entity Manager
+        $manager = $doctrine->getManager();
+        // demande d'enregistrement de l'objet dans la BDD
+        $manager->persist($livre1);
+        $manager->flush();
+        dump($livre1);
+
+        // * Requêtes de mise à jour :
+
+        // modification d'un objet
+
+        // recuperation d'un livre par son ID
+        $livre2 = $livreRepository->find(2);
+        // changment de titre 
+        $livre2->setTitre('Aperiendum est igitur');
+        // recuperation de genre par son ID
+        $genre = $genreRepository->find(2);
+        // suppression genre
+        $livre2->removeGenre($genre);
+        // recuperation de genre par son ID
+        $genre = $genreRepository->find(5);
+        // Ajout nouvelle genre
+        $livre2->addGenre($genre);
+
+        // enregistrement de la modification dans la BDD
+        $manager->flush();
         
-        foreach ($livres as $livre){
-            dump($livre);
+        dump($livre2);
 
-            $genres = $livre->getGenres();
+        // * Requêtes de suppression :
+        $id = 123;
+        $livre3 = $livreRepository->find($id);
 
-            foreach($genres as $genre) {
-                dump($genre);
-            }
+        if ($livre3) {
+            // suppression d'un objet
+            $manager->remove($livre3);
+            $manager->flush();
         }
-        
-
-
-
+        dump($livre3);
 
         exit();
     }
